13b38aa1078f4f3f8b7a7e7ef88e28066abfe023
Joseph Choi <jdc07@live.com>
2020-02-23 17:55:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,16 +27,12 @@ monopoly = {
       pieces: 4,
       names: {
         :reading_railroad => {mortgage_value:100
-          ...
           },
-        :pennsylvania_railroad => {
-          ...
+        :pennsylvania_railroad => {mortgage_value:200
           },
-          :b_and_o_railroad => {
-           ...
+          :b_and_o_railroad => {mortgage_value:400
           },
-          :shortline => {
-            ...
+          :shortline => {mortgage_value:80
           }
       },
       rent_in_dollars: {
